24ed019477bacd3d8e5d4f4ea2b09b7cea87ccde
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2023-09-20 14:41:36 +0900

    Initial Commit

---
+++ workbook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  workbook
+//
+//  Created by 정민지 on 2023/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ workbookUITests/workbookUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  workbookUITestsLaunchTests.swift
+//  workbookUITests
+//
+//  Created by 정민지 on 2023/09/20.
+//
+
+import XCTest
+
+final class workbookUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
